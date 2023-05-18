0f023b120d79c320fb53ff43ef1eec2573d61004
chantifa <chantifa@windowslive.com>
2023-05-18 14:59:05 +0200

    update

--- clco/src/main/java/ch/ffhs/clco/MvcConfig.java
+++ clco/src/main/java/ch/ffhs/clco/MvcConfig.java
@@ -1,10 +1,12 @@
 package ch.ffhs.clco;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ComponentScan
 public class MvcConfig implements WebMvcConfigurer {
 
     @Override
--- clco/src/main/java/ch/ffhs/clco/common/TextToJsonConverter.java
+++ clco/src/main/java/ch/ffhs/clco/common/TextToJsonConverter.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Function;
 
 @Component
 public class TextToJsonConverter {
